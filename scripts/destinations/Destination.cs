2692ac1f0c798ad88659361eb3915fea91520a7f
Re-Programmed <79171779+Re-Programmed@users.noreply.github.com>
2022-04-12 19:04:42 -0600

    Create Destination.cs

---
+++ scripts/destinations/Destination.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+#pragma warning disable
+
+public abstract class Destination : MonoBehaviour
+{
+
+}
