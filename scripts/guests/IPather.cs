a6125600da65a74512945501c3729a278473f417
Re-Programmed <79171779+Re-Programmed@users.noreply.github.com>
2022-04-12 18:08:13 -0600

    Update IPather.cs

--- scripts/guests/IPather.cs
+++ scripts/guests/IPather.cs
@@ -1,1 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+using TMPro;
+
+#pragma warning disable
+
+public interface IPather
+{
+  public abstract void Path(Vector3 location);
+  public abstract void PathTick(int t);
+}
